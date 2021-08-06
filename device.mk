37eebfbcb532bf944b9d00aaf27b0bc9f4e8251b
sairam1411 <majgaonkars3@gmail.com>
2021-08-06 20:39:37 +0530

    Revert "land: Switch to audio 6.0"
    All devices use pre-R audio.bluetooth_qti.default.so and land uses
    pre-R audio hal, in which the new set_event_callback API is not
    implemented and the audio service will crash due to mismatched struct.
    
    This reverts commit 475faefb9dc5ba2b916b60571063f53f88661665.

--- device.mk
+++ device.mk
@@ -69,13 +69,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio@6.0 \
+    android.hardware.audio@5.0 \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio.common@5.0 \
     android.hardware.audio.common@5.0-util \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0 \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@5.0 \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     android.hardware.soundtrigger@2.2-service
 
